10e290a1aaa064cc967cae1da703bb8f0f2ab9c1
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-19 21:27:26 +0500

    refactor: removing all files for reinstalling

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import Vue3Editor from "vue3-editor";
-
-createApp(App).use(store).use(router).use(Vue3Editor).mount("#app");
