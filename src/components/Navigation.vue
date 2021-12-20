9fa6185f5da13a6d27a5e3507238b1805f1b5f89
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-20 15:35:36 +0500

    feat: adding some styles to navigation component

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -36,4 +36,39 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  background-color: #fff;
+  padding: 0 25px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  z-index: 99;
+
+  .link {
+    font-weight: 500;
+    padding: 0 8px;
+    transition: 0.3s color ease;
+
+    &:hover {
+      color: #1eb8b8;
+    }
+  }
+
+  nav {
+    display: flex;
+    padding: 25px 0;
+
+    .branding {
+      display: flex;
+      align-items: center;
+
+      .logo {
+        font-weight: 600;
+        font-size: 24px;
+        color: #000;
+        text-decoration: none;
+      }
+    }
+  }
+}
+</style>
