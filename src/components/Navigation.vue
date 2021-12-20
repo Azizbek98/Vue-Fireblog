64853e9487b33bda02fa4a275cd22f8afadffa1f
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-20 15:04:58 +0500

    feat: adding navigation component to the project

---
+++ src/components/Navigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <nav class="container">
+      <div class="branding">
+        <router-link class="logo" :to="{ name: 'Home' }">FireBlogs</router-link>
+      </div>
+      <div class="nav-links">
+        <router-link class="link" to="#">Home</router-link>
+        <router-link class="link" to="#">Blogs</router-link>
+        <router-link class="link" to="#">Create Post</router-link>
+        <router-link class="link" to="#">Login / Register</router-link>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
