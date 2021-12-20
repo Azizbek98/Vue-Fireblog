c9d4a19ab6d1c81a09b245b72ecbf87039ac529e
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-20 17:34:41 +0500

    feat: adding logic to the navigation component

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,7 +5,7 @@
         <router-link class="logo" :to="{ name: 'Home' }">FireBlogs</router-link>
       </div>
       <div class="nav-links">
-        <ul>
+        <ul v-show="!mobileNav">
           <router-link class="link" to="#">Home</router-link>
           <router-link class="link" to="#">Blogs</router-link>
           <router-link class="link" to="#">Create Post</router-link>
@@ -13,9 +13,9 @@
         </ul>
       </div>
     </nav>
-    <menuIcon class="menu-icon" />
+    <menuIcon @click="toggleMobileNav" v-show="mobile" class="menu-icon" />
     <transition>
-      <ul class="mobile-nav">
+      <ul class="mobile-nav" v-show="mobileNav">
         <router-link class="link" to="#">Home</router-link>
         <router-link class="link" to="#">Blogs</router-link>
         <router-link class="link" to="#">Create Post</router-link>
@@ -32,6 +32,37 @@ export default {
   name: "Navigation",
   components: {
     menuIcon,
+  },
+  // Data function for returning models
+  data() {
+    return {
+      mobile: null,
+      mobileNav: null,
+      windowWidth: null,
+    };
+  },
+  // Lifecycle hooks
+  created() {
+    window.addEventListener("resize", this.checkScreen);
+    this.checkScreen();
+  },
+
+  // Functions
+  methods: {
+    checkScreen() {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth < 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+      this.mobileNav = false;
+      return;
+    },
+
+    toggleMobileNav() {
+      this.mobileNav = !this.mobileNav;
+    },
   },
 };
 </script>
