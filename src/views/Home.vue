d6a73d99f4a813f04098595bba44cecef91aab2f
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-21 17:24:01 +0500

    feat: adding info of the home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,5 +6,28 @@
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome !",
+        blogPost:
+          "Weekly blog articles with all things programming including HTML, CSS, JS and more. Register today to never miss a post.",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "This is a Filter Title !",
+          blogHTML: "This is blog post title",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "This is a Filter Title !",
+          blogHTML: "This is blog post title",
+          blogCoverPhoto: "beautiful-stories",
+        },
+      ],
+    };
+  },
 };
 </script>
