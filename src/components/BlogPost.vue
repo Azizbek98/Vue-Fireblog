b9edb1c0e9d53d3218b29adbe5739f570278b1a1
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-23 22:34:29 +0500

    feat: adding some changes to blog post component

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="blog-wrapper">
-    <div class="blog-content"></div>
+    <div class="blog-content">
+      <div>
+        <h2 v-if="post.welcomeScreen">{{ post.title }}</h2>
+        <h2 v-else>{{ post.title }}</h2>
+      </div>
+    </div>
   </div>
 </template>
 
