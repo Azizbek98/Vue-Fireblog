703d5d9c5ced5c2889bd0ddd8a8beda4a69bcae5
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-24 11:43:16 +0500

    feat: adding some styles to blog post component

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -43,4 +43,12 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.blog-wrapper {
+  display: flex;
+  flex-direction: column;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  @media (min-width: 800px) {
+  }
+}
+</style>
