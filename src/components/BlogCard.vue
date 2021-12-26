649fdc252c4673c13d713f4c2dd854709e7ee69c
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-26 23:10:21 +0500

    feat: adding icons for blog card component

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -3,7 +3,19 @@
 </template>
 
 <script>
-export default {};
+import Arrow from "../assets/Icons/arrow-right-light.svg";
+import Edit from "../assets/Icons/edit-regular.svg";
+import Delete from "../assets/Icons/trash-regular.svg";
+
+export default {
+  name: "BlogCard",
+  props: ["post"],
+  components: {
+    Arrow,
+    Edit,
+    Delete,
+  },
+};
 </script>
 
 <style></style>
