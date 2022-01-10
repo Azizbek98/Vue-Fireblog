51cce41f9abd29bca82db34c89f78d03e0f130db
Azizbek Samatov <azizbeksamatov98@gmail.com>
2022-01-10 10:38:55 +0500

    feat: adding initial structure to forgot password component

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,9 +1,29 @@
 <template>
-  <div></div>
+  <div class="reset-password">
+    <div class="form-wrap">
+      <form class="reset">
+        <h2>Reset Password</h2>
+        <p>Forgot your password? Enter your email to reset it.</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email" />
+            <Email class="icon" />
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import Email from "../assets/Icons/envelope-regular.svg";
+
+export default {
+  name: "ForgotPassword",
+  components: {
+    Email,
+  },
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped></style>
