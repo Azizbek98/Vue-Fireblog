b4e51560e9945d83ce5893861aed8e8fe0d62869
Azizbek Samatov <azizbeksamatov98@gmail.com>
2022-01-10 10:15:04 +0500

    feat: adding some changes to register component

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,13 +2,25 @@
   <div class="form-wrap">
     <form class="register">
       <p class="login-register">
-        Don't have an account?
-        <router-link class="router-link" :to="{ name: 'Register' }">
-          Register
+        Already have an account?
+        <router-link class="router-link" :to="{ name: 'Login' }">
+          Login
         </router-link>
       </p>
-      <h2>Login to FireBlogs</h2>
+      <h2>Create Your FireBlogs Account</h2>
       <div class="inputs">
+        <div class="input">
+          <input type="text" placeholder="First Name" v-model="firstName" />
+          <User class="icon" />
+        </div>
+        <div class="input">
+          <input type="text" placeholder="Last Name" v-model="lastName" />
+          <User class="icon" />
+        </div>
+        <div class="input">
+          <input type="text" placeholder="Username" v-model="username" />
+          <User class="icon" />
+        </div>
         <div class="input">
           <input type="text" placeholder="Email" v-model="email" />
           <Email class="icon" />
@@ -18,10 +30,7 @@
           <Password class="icon" />
         </div>
       </div>
-      <router-link class="forgot-password" :to="{ name: 'ForgotPassword' }">
-        Forgot password
-      </router-link>
-      <button>Sign In</button>
+      <button>Sign Up</button>
       <div class="angle"></div>
     </form>
     <div class="background"></div>
@@ -29,8 +38,17 @@
 </template>
 
 <script>
+import Email from "../assets/Icons/envelope-regular.svg";
+import Password from "../assets/Icons/lock-alt-solid.svg";
+import User from "../assets/Icons/user-alt-light.svg";
+
 export default {
   name: "Register",
+  components: {
+    Email,
+    Password,
+    User,
+  },
 };
 </script>
 
